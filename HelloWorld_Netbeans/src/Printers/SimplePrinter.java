a124892a1b0f35af738f8426b26755670fbc3497
Paolo Buzzi <paolo.buzzi1984@gmail.com>
2018-02-07 09:08:44 +0100

    Added Printers.

---
+++ HelloWorld_Netbeans/src/Printers/SimplePrinter.java
@@ -0,0 +1,20 @@
+
+package Printers;
+
+
+public class SimplePrinter {
+    
+    String name;
+    
+    public SimplePrinter(String str) {
+    
+        this.name=str;
+        System.out.println("Hi, I'm "+this.name+", a SimplePrinter!");
+    }
+    
+    public void printSecretMessage(String message){
+    
+        System.out.println("The secret message is: "+ message+ "\n");
+
+    }
+}
